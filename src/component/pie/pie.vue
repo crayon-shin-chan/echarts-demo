b3febbf3ec33b7008633facf2e618a3b1e4f6510
chenzhenyuan@skyable.cn <1253665625@qq.com>
2018-09-21 16:28:26 +0800

    各个选项测试

--- src/component/pie/pie.vue
+++ src/component/pie/pie.vue
@@ -8,160 +8,217 @@
 
 <script lang="ts">
 
-import Vue from 'vue'
+import { ECharts,EChartOption,EChartTitleOption,init } from 'echarts'
 import Component from 'vue-class-component'
-import * as echarts from 'echarts'
+import Vue from 'vue'
 
 /** 虽然Component装饰器中包含各种属性，data、method等，但是在装饰器参数中定义vue组件属性并不合适 */
 @Component({})
 export default class Pie extends Vue{
 
-    /** echarts选项 */
-    option:echarts.EChartOption = {
-                title:{
-                    top: '40',
-                    left: '30',
-                    text: '人口分布',
-                    textStyle: {
-                        color: 'black',
-                        fontSize: 20,
-                        fontFamily: 'serif',
-                        fontWeight: 'bold',
-                        align: 'left',
-                        verticalAlign: 'middle',
-                        lineHeight: 60,
-                        textBorderWidth:2,
-                        textBorderColor:'white'
-                    }
-                },
-                legend:{
-                    type:"plain"
-                },
-                grid: {
-                    left: '3%',
-                    right: '20%',
-                    top: '8%',
-                    containLabel: true,
-                    show:false
-                }, 
-                backgroundColor: 'white',
-                visualMap: [
-                    {
-                        show: false,
-                        min: 500,
-                        max: 600,
-                        inRange: {
-                            colorLightness: [0, 1]
-                        }
-                    }
-                ],
-                tooltip:{
-                    show:false
-                },
-                series : [
-                    {
-                        name:'人口分布',
-                        type:'pie',
-                        clockwise:'true',
-                        startAngle:'0',
-                        radius : 120,
-                        center: ['50%', '50%'],
-                        data:[
-                            {
-                                name:'亚洲',
-                                value:30,
-                                label:{
-                                    show:true,
-                                    position:'outside'
-                                },
-                                labelLine:{
-                                    smooth:false
-                                },
-                                itemStyle:{
-                                    color: 'red',
-                                    normal:{
-                                        color:'yellow',
-                                    },
-                                    borderColor: 'white',
-                                    borderWidth: 2
-                                },
-                                emphasis:{
-                                    itemStyle:{
-                                        color:'yellow'
-                                    }
-                                }
-                            },
-                            {
-                                name:'欧洲',
-                                value:10,
-                                itemStyle:{
-                                    normal:{
-                                        color:'green'
-                                    }
-                                },
-                                emphasis:{
-                                    itemStyle:{
-                                        color:'green'
-                                    }
-                                }
-                            },
-                            {
-                                name:'美州',
-                                value:10,
-                                itemStyle:{
-                                    normal:{
-                                        color:'blue'
-                                    }
-                                },
-                                emphasis:{
-                                    itemStyle:{
-                                        color:'blue'
-                                    }
-                                }
-                            },
-                            {
-                                name:'非洲',
-                                value:10,
-                                itemStyle:{
-                                    normal:{
-                                        color:'black'
-                                    }
-                                },
-                                emphasis:{
-                                    itemStyle:{
-                                        color:'black'
-                                    }
-                                }
-                            }
-                        ],
-                    }
-                ]
-            };
+    initOptions:{
+        devicePixelRatio?: number
+        renderer?: string
+        width?: number | string
+        height?: number | string
+    } = {
+        /** 设备像素比 */
+        devicePixelRatio: window.devicePixelRatio,
+        /** 渲染器，canvas、svg */
+        renderer: "canvas",
+        width:'auto',
+        height:'auto'
+    }
 
-    instance:echarts.ECharts;
+    /** 标题选项 */
+    title:EChartTitleOption = {
+        top: '40',
+        left: '30',
+        text: '人口分布',
+        textStyle: {
+            color: 'black',
+            fontSize: 20,
+            fontFamily: 'serif',
+            fontWeight: 'bold',
+            align: 'left',
+            verticalAlign: 'middle',
+            lineHeight: 60,
+            textBorderWidth:2,
+            textBorderColor:'white'
+        }
+    }
+
+    /** 图例选项 */
+    legend:object = {
+        type:"plain"
+    }
+
+    /** 提示网格选项 */
+    grid:object = {
+        left: '3%',
+        right: '20%',
+        top: '8%',
+        containLabel: true,
+        show:false
+    }
+
+    /** 虚拟映射 */
+    visualMap:object[] = [
+        {
+            show: false,
+            min: 500,
+            max: 600,
+            inRange: {
+                colorLightness: [0, 1]
+            }
+        }
+    ]
+
+    /** 提示工具 */
+    tooltip: object = {
+        show:false
+    }
+
+    /** 扇形数据数据数组 */
+    data: object[] = [
+        {
+            name:'亚洲',
+            value:30,
+            label:{
+                show:true,
+                position:'outside'
+            },
+            labelLine:{
+                smooth:false
+            },
+            itemStyle:{
+                color: 'red',
+                normal:{
+                    color:'yellow',
+                },
+                borderColor: 'white',
+                borderWidth: 2
+            },
+            emphasis:{
+                itemStyle:{
+                     color:'yellow'
+                }
+            }
+        },
+        {
+            name:'欧洲',
+            value:10,
+            itemStyle:{
+                normal:{
+                    color:'green'
+                }
+            },
+            emphasis:{
+                itemStyle:{
+                    color:'green'
+                }
+            }
+        },
+        {
+            name:'美州',
+            value:10,
+            itemStyle:{
+                normal:{
+                    color:'blue'
+                }
+            },
+            emphasis:{
+                itemStyle:{
+                    color:'blue'
+                }
+            }
+        },
+        {
+            name:'非洲',
+            value:10,
+            itemStyle:{
+                normal:{
+                    color:'black'
+                }
+            },
+            emphasis:{
+                itemStyle:{
+                    color:'black'
+                }
+            }
+        }
+    ];
+
+    series1:object = {
+        name:'人口分布',
+        type:'pie',
+        
+        legendHoverLink:false,
+        /** 图例覆盖时的放大动画效果 */
+        hoverAnimation:false,
+        /** 图例覆盖时，放大的偏移距离 */
+        hoverOffset:200,
+        /** 是否支持多选中 */
+        selectedMode:false,
+        /** 多选中时，的扇区偏移 */
+        selectedOffset:40,
+        /** 扇区是否按照顺时针排布 */
+        clockwise:true,
+        /** 起始角度 */
+        startAngle:0,
+        /** 最小扇区角度，为了防止扇区角度过小，影响交互，如果小于这个角度，则会强行放大角度 */
+        minAngle:30,
+        /** 是否展示未南丁格尔图，通过半径区分数据大小，radius、area */
+        roseType:false,
+        /** 是否启用防止标签重叠策略 */
+        avoidLabelOverlap:true,
+        /** 是否在数据和为0时不显示扇区 */
+        stillShowZeroSum:true,
+        /** 鼠标悬浮时的样式 */
+        cursor:'pointer',
+        center: ['50%', '50%'],
+        radius : 120,
+        /** 映射数据集时，决定用行还是列来映射 */
+        seriesLayoutBy:'column',
+        /** 图标数据 */
+        data: this.data,
+        /** 图表标注 */
+        markPoint:{
+            /** 标注的图形 */
+            symbol:'pin'
+        }
+    }
+
+    /**  */
+    series :object[] = [ this.series1 ]
+
+    /** echarts选项 */
+    option:EChartOption = {
+        title : this.title,
+        legend : this.legend,
+        grid : this.grid, 
+        backgroundColor: 'white',
+        visualMap: this.visualMap,
+        tooltip: this.tooltip,
+        series : this.series
+    };
+
+    instance:ECharts;
 
     $refs:{
         pie:HTMLDivElement
     } ;
 
     mounted(){
-         this.instance = echarts.init(this.$refs.pie,{
-            /** 设备像素比 */
-            devicePixelRatio: window.devicePixelRatio,
-            /** 渲染器，canvas、svg */
-            renderer: "canvas",
-            width:'auto',
-            height:'auto'
-        });
-        this.instance.setOption(this.$data.option,true,true)
+        this.instance = init(this.$refs.pie,this.initOptions);
+        this.instance.setOption(this.option,true,true)
     }
-
 }
-
 
 </script>
 
 <style lang="scss">
+
     .pie-container{
         width:100%;
         height:100%;
